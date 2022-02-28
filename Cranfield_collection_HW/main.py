a4a917b36ee56e9d72f416a0f021dc7189577cb1
Sylv <sc8043@nyu.edu>
2022-02-28 12:50:00 -0500

    IR output

--- Cranfield_collection_HW/main.py
+++ Cranfield_collection_HW/main.py
@@ -13,6 +13,10 @@ import sys
 queries = []
 total_length = 0
 queries_TF = {}
+queries_IDF = {}
+queries_TFIDF = {}
+query_filtered_words = []
+
 
 
 # construction of query TFIDS
@@ -21,7 +25,7 @@ with open("cran.qry","r") as file:
     lines = file.readlines()
     init = ""
     for line in lines:
-        if(line[0]!="."): init+=line
+        if(line[0]!="."): init+=line.strip('\n\r')+" "
         else: 
             if(init!=""):
                 queries.append(init)
@@ -30,33 +34,48 @@ with open("cran.qry","r") as file:
 
 
 for index,value in enumerate(queries):
+    query_filtered = []
     query = value.split(' ')
-    copy = []
     for word in query:
-        if (word not in closed_class_stop_words) & (re.findall('0-9',word) is None): #get rid of numbers
-            copy.append(word)
-    queries_TF[index] = dict.fromkeys(copy,0)
+        if (word not in closed_class_stop_words)& (re.findall(r'[\!\(\)\-\[\]\{\}\;\:\'\"\,\<\>\=\.\/\?\@\#\$\%\^\&\*\_\~]',word)==[])& (re.findall(r'[0-9]',word)==[]):
+            query_filtered.append(word)
+    queries_TF[index] = dict.fromkeys(set(query_filtered),0)
+    queries_TFIDF[index] = dict.fromkeys(set(query_filtered),0)
+    query_filtered_words.append(query_filtered)
+
+
+total_queries = len(queries)
+    
 
 for index,query in enumerate(queries):
     for word in query.split(" "):
-        if word not in closed_class_stop_words:
-            for x in range(len(queries)):
-                if queries_TF.get(x).get(word) is not None:
-                    queries_TF[x][word]+=1
+        word = word.strip("\n\r ,./?")
+        if word in queries_TF.get(index).keys():
+            queries_TF[index][word] += 1
             
 
-for key,value in queries_TF.items():
-    for word, fre in value.items():
-        queries_TF[key][word] = math.log(total_length / fre)
+def IDF_query(word):
+    counter = 0
+    for list in query_filtered_words:
+        if word in list: counter +=1
+    return counter
 
+for key, value in queries_TF.items():
+    for word, times in value.items():
+        queries_IDF[word] = math.log(total_queries/IDF_query(word))
+        queries_TFIDF[key][word] = times * math.log(total_queries/IDF_query(word))
 
-
-    
+# for k,v in queries_TFIDF.items():
+#     print(k,v)
 
 # construction of document TFIDS 
 
 documents = []
-abstract_freq = {}
+abstract_TF = {}
+abstract_IDF = {}
+abstract_TFIDF = {}
+abstract_filtered_words = []
+
 
 
 # with open (sys.argv[0],'r') as file:
@@ -77,28 +96,97 @@ with open('cran.all.1400',"r") as file:
 documents = documents[1:] # remove first empty element
 
 
-documents_filtered = []
 
 for index,abstract in enumerate(documents):
+    documents_filtered = []
     for word in abstract.split(" "):
         word = word.strip(".,?/!\'\"\\ ")
         if (word not in closed_class_stop_words)& (re.findall(r'[\!\(\)\-\[\]\{\}\;\:\'\"\,\<\>\=\.\/\?\@\#\$\%\^\&\*\_\~]',word)==[])& (re.findall(r'[0-9]',word)==[]):
             documents_filtered.append(word)
     no_duplicate = set(documents_filtered)
-    abstract_freq = dict.fromkeys(no_duplicate,0)
+    abstract_TF[index] = dict.fromkeys(no_duplicate,0)
+    abstract_TFIDF[index] = dict.fromkeys(no_duplicate,0)
+    abstract_filtered_words.append(documents_filtered)
 
-def counter_freq_doc(word):
+
+
+for index, abstract in enumerate(documents):
+    for word in abstract.split(" "):
+        word = word.strip("\n\r ,./?")
+        if word in abstract_TF.get(index).keys():
+            abstract_TF[index][word]+=1
+
+def IDF_abstract(word):
     counter = 0
-    for item in documents_filtered:
-        if word == item: counter+=1
+    for list in abstract_filtered_words:
+        if word in list: counter +=1
     return counter
 
-total_document_word = len(documents_filtered)
 
-for word in no_duplicate:
-    abstract_freq[word] = round(math.log(total_document_word/counter_freq_doc(word)),5)
+total_abstracts = len(documents)
+# print(total_abstracts)
+
+for key, value in abstract_TF.items():
+    for word, times in value.items():
+        abstract_IDF[word] = math.log(total_abstracts/IDF_abstract(word))
+        abstract_TFIDF[key][word] = times * math.log(total_abstracts/IDF_abstract(word))
+
+# for k,v in abstract_TFIDF.items():
+#     print(k,v)
+
+
+# calculate cosine similarities 
+
+
+def cosine_similarity(query,document):
+    numerator = 0
+    denominator_q = 0
+    denominator_d = 0
+    # print(query,document)
+    for index,score in enumerate(query):
+        # print(index,score)
+        if score != 0:
+            numerator += score*document[index]
+            denominator_q += math.pow(score,2)
+        denominator_d += math.pow(document[index],2)
+    # print(numerator, math.sqrt(denominator_d*denominator_q))
+    if(math.sqrt(denominator_d*denominator_q)!= 0): return (numerator / math.sqrt(denominator_d*denominator_q))
+    else: return 0
 
 
 
-# calculation
+def calculate_vectors(query,index):
+    abstract_rank = {}
+    for key, value in abstract_TFIDF.items():
+        q_vector = []
+        a_vector = []
+        for q in query:
+            if q in value.keys():
+                q_vector.append(queries_TFIDF[index][q])
+                a_vector.append(value.get(q))
+        cosine = cosine_similarity(q_vector,a_vector)
+        abstract_rank[key] = cosine
+    return abstract_rank
+
+
+output = {}
+
+for index, query in queries_TFIDF.items():
+    output[index] = calculate_vectors(query, index)
+
+
+with open("output.txt","w") as file:
+    for k,v in output.items():
+        for key, value in sorted(v.items(), key=lambda x:x[1],reverse=True):
+            file.write(str(k+1) + " " + str(key) + " " + str(value))
+            file.write('\n')
+    file.close()
+
+
+    
+
+
+
+
+
 
